6a99ea2a0d246af57f15fddfc1864583fa33e6cc
daophanquochoai <hoai23828@mgail.com>
2025-02-27 13:56:29 +0700

    FIX : kafke connect to service, FIX : zipkin connect to service, FIX : convert file k8s

--- paymentservice/src/main/java/doctorhoai/learn/paymentservice/config/KafkaConfig.java
+++ paymentservice/src/main/java/doctorhoai/learn/paymentservice/config/KafkaConfig.java
@@ -1,10 +1,21 @@
 package doctorhoai.learn.paymentservice.config;
 
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
--- proxy_client/src/main/java/doctorhoai/learn/proxy_client/business/user/service/AccountBankFeign.java
+++ proxy_client/src/main/java/doctorhoai/learn/proxy_client/business/user/service/AccountBankFeign.java
@@ -7,11 +7,12 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @FeignClient(name = "userservice", contextId = "accountBankProxyClient", path = "/account/bank", fallbackFactory = AccountBankFeignFallBack.class)
+//@RibbonClient(name = "userservice") // TODO: deploy k8s
 public interface AccountBankFeign {
     @PostMapping("/add")
     public ResponseEntity<Response> addAccountBank(
